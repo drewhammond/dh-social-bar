486bf689f9c34a167f3c707271b5947a42b32c45
Drew Hammond <drew@marketwarp.com>
2014-01-28 10:05:50 -0500

    Menu position fixing

--- js/script.js
+++ js/script.js
@@ -6,15 +6,32 @@ jQuery(document).ready(function ($) {
 
 	// Personal use for now; determine height of navbar. Subtract height of bar and padding of hidden element
 	var navHeight = $('nav.navbar').height();
-	var expandedHeight = (windowHeight - 50 - 32 - navHeight);
+	var expandedHeight = (windowHeight - 50 - navHeight);
+	var fixedNavOffset = windowHeight - navHeight;
+	// Hack to track toggle status because I cant remember how to do it right with jQuery
+	var sibIsOpen = false;
+
+	// CSS classes for positioning the menu
+	$('head').append('<style>.sibFixNav { top: ' + fixedNavOffset + 'px !important; }');
 
 	console.log(windowHeight);
 
 	$('.sib-toggle > a').click(function (e) {
 		e.preventDefault();
-		$('.sib-teaser').fadeToggle();
-		$('.sib-hidden').slideToggle('normal', function () {
+		sibIsOpen.togg
 
+		console.log(sibIsOpen);
+
+		$('.sib-teaser').fadeToggle();
+		$('nav.navbar').toggleClass('sib-fixed sibFixNav');
+
+		// Fix navbar to bottom of screen
+		//$('nav.navbar').css({ top: windowHeight - navHeight });
+
+		// Expand the hidden div
+		$('.sib-hidden').slideToggle('normal', function (e) {
+
+			console.log($(this));
 			// Change display;
 			$('.sib-wrapper').toggleClass('sib-fixed');
 			$('.sib-toggle > a').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
@@ -22,7 +39,7 @@ jQuery(document).ready(function ($) {
 			// Set height of expanded div based on calculations above
 			$('.sib-hidden').css('height', expandedHeight);
 		})
-	})
+	});
 
 });
 
