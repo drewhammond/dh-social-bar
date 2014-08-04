edf6282bff0813c408e5b7f01ce5d7f7c8f78346
Drew Hammond <drew@alphagenetica.com>
2014-08-04 16:45:42 -0400

    #1 Renamed plugin class, updated comments, and refactored minor details

--- README.md
+++ README.md
@@ -2,17 +2,26 @@
 Contributors: DrewHammond
 Tags: responsive, social, facebook, twitter, calendar
 Requires at least: 3.5
-Tested up to: 3.8.2
+Tested up to: 3.9.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 Displays social feeds and other goodies at the top of your blog in a responsive dropdown
 
 == Description ==
+
 The Social Integration bar is a WordPress plugin that affixes a bar to the top of your site with content pulled from your Twitter, Facebook, and Calendar feed.
 
 It also includes links to social media profiles
 
 == Changelog ==
-0.1 - First tracked revision
+
+= 1.0.0 =
+* BREAKING: Renamed plugin class from DH_Social_Bar to DH_Social_Integration_Bar
+* Renamed primary plugin file
+* Moved to semantic versioning
+* Removed local loading of jQuery
+
+= 0.1.0 =
+* First tracked revision
 
--- social-integration-bar.php
+++ social-integration-bar.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name: Social Integration Bar
-Version: 0.1
-Plugin URI: https://github.com/drewhammond/dh-social-bar/
+Version: 1.0.0
+Plugin URI: https://github.com/drewhammond/social-integration-bar/
 Description: Displays social feeds and other goodies at the top of your blog in a responsive dropdown
-Author: Drew Hammond <drew@marketwarp.com>
-Author URI: http://www.marketwarp.com
+Author: Drew Hammond <drew@alphagenetica.com>
+Author URI: http://www.alphagenetica.com
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
-    Copyright (C) 2014  Drew Hammond <drew@marketwarp.com>
+    Copyright (C) 2014  Drew Hammond <drew@alphagenetica.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,17 +27,19 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 /**
- * Class DH_Social_Bar
+ * Class DH_Social_Integration_Bar
+ *
  *
  * @package Social Integration Bar
  * @requires Genesis Theme for WP (studiopress.com)
  *
- * @since 0.1
+ * @since 1.0.0
  */
-class DH_Social_Bar {
+class DH_Social_Integration_Bar {
 
 
-	private static $ns = 'social-integration-bar';
+	const NS = 'social-integration-bar';
+	const VERSION = '1.0.0';
 	protected $num_widgets = 2;
 	/**
 	 * @var array Default configuration array
@@ -53,8 +55,8 @@ class DH_Social_Bar {
 	public function __construct() {
 
 		// Define paths
-		define('SIB_PLUGIN_URL', plugin_dir_url(__FILE__));
-		define('SIB_PLUGIN_VER', '0.4');
+		define( 'SIB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		define( 'SIB_PLUGIN_VER', self::VERSION );
 
 		// Leeeeeeroyyyyy Jenkinsssss
 		$this->init();
@@ -68,16 +70,16 @@ class DH_Social_Bar {
 	public function init() {
 		// Do plugin initialization
 
-		add_action('wp_enqueue_scripts', array($this, 'init_css'));
-		add_action('wp_enqueue_scripts', array($this, 'init_js'));
-		add_action('wp_footer', array($this, 'render'));
+		add_action( 'wp_enqueue_scripts', array( $this, 'init_css' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'init_js' ) );
+		add_action( 'wp_footer', array( $this, 'render' ) );
 
 
-		$this->register_widgets(2);
+		$this->register_widgets( 2 );
 
 
 		//$theme = new acv3_theme();
-		if (method_exists('acv3_theme', 'render_social_icons')) {
+		if ( method_exists( 'acv3_theme', 'render_social_icons' ) ) {
 
 		}
 
@@ -98,15 +100,15 @@ class DH_Social_Bar {
 	 *
 	 * @param int $quantity Number of widget boxes to register in the Social Integration Bar
 	 */
-	public function register_widgets($quantity = 2) {
+	public function register_widgets( $quantity = 2 ) {
 
 
-		for ($i=0; $i<$quantity; $i++) {
+		for ( $i = 0; $i < $quantity; $i ++ ) {
 			register_sidebar( $args = array(
-				'name' 			=> __( 'Social Integration Box ' .  ($i+1), 'marketwarp'),
-				'id' 			=> self::$ns . 'widget-area-' . $i,
-				'description' 	=> __( 'This widget area is displayed in the left hand column on the Social Integration Bar', 'marketwarp' ),
-				'class' 		=> self::$ns . '-section-' . ($i+1),
+				'name'          => __( 'Social Integration Box ' . ( $i + 1 ), self::NS ),
+				'id'            => self::NS . 'widget-area-' . $i,
+				'description'   => __( 'This widget area is displayed in the left hand column on the Social Integration Bar', self::NS ),
+				'class'         => self::NS . '-section-' . ( $i + 1 ),
 				'before_widget' => '<li id="%1$s" class="widget %2$s list-unstyled">',
 				'after_widget'  => '</li>',
 				'before_title'  => '<p class="sib-widget-title">',
@@ -118,7 +120,8 @@ class DH_Social_Bar {
 
 	}
 
-	public function render() { ?>
+	public function render() {
+		?>
 
 		<div class="social-integration-bar sib-wrapper">
 			<div class="sib-inner-wrapper">
@@ -131,10 +134,10 @@ class DH_Social_Bar {
 					</div>
 					<div class="sib-hidden container">
 						<div class="col-md-6">
-							<?php dynamic_sidebar('Social Integration Box 1'); ?>
+							<?php dynamic_sidebar( 'Social Integration Box 1' ); ?>
 						</div>
 						<div class="col-md-6">
-							<?php dynamic_sidebar('Social Integration Box 2'); ?>
+							<?php dynamic_sidebar( 'Social Integration Box 2' ); ?>
 						</div>
 					</div>
 				</div>
@@ -145,13 +148,12 @@ class DH_Social_Bar {
 	}
 
 	public function init_css() {
-		wp_register_style('social-integration-bar', SIB_PLUGIN_URL . 'css/styles.css');
-		wp_enqueue_style('social-integration-bar');
+		wp_register_style( 'social-integration-bar', SIB_PLUGIN_URL . 'css/styles.css' );
+		wp_enqueue_style( 'social-integration-bar' );
 	}
 
 	public function init_js() {
-		wp_enqueue_script('jquery', SIB_PLUGIN_URL . 'js/jquery-1.11.0.min.js');
-		wp_enqueue_script('social-integration-bar', SIB_PLUGIN_URL . 'js/script.js', array('jquery'), SIB_PLUGIN_VER, true);
+		wp_enqueue_script( 'social-integration-bar', SIB_PLUGIN_URL . 'js/script.js', array( 'jquery' ), SIB_PLUGIN_VER, true );
 	}
 
 }
@@ -162,10 +164,10 @@ class DH_Social_Bar {
  *
  * @since 0.1
  */
-function dh_social_bar_init() {
+function dh_social_integration_bar_init() {
 	// Create new instance of Social Bar
-	$dh_social_bar = new DH_Social_Bar();
+	$dh_social_bar = new DH_Social_Integration_Bar();
 }
 
 
-add_action( 'init', 'dh_social_bar_init', 20 );
+add_action( 'init', 'dh_social_integration_bar_init', 20 );
